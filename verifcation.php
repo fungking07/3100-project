5da621f8149c9225594f32a00ece9f587d1d8e53
henryL0-0 <49423066+henryL0-0@users.noreply.github.com>
2020-04-19 23:05:46 +0800

    Update verifcation.php
    debug left, do it after a properly account is inserted(maybe)

--- verifcation.php
+++ verifcation.php
@@ -17,10 +17,17 @@
     $userdata = mysqli_fetch_all($result,MySQLI_ASSOC);
     if ($result == NULL){
         // red message of user not exist
-        echo "user not exist.";
+        echo "user with you email address not exist.";
     }
     else{
         $code = substr(md5(uniqid(rand(), true)), 8, 8);
+        $sql = "UPDATE user SET verify_code ='$code' WHERE email_address = '$email'";
+        if(mysqli_query($connect,$sql)){
+            echo "code set in database.";
+        }
+        else{
+            echo "Error: set code";
+        }
         //email
         $email_to = "$email";
         $email_subject = "Please Verify Your AcadMap Account";
@@ -30,51 +37,13 @@
                 \n\n$code\n\nThank you.\n\nRegrads,\nThe AcadMap team";
                 
         mail($email,$email_subject,$message,[$header]);
-        $verify_code = $_POST["code"]; //input = code
-        $verify_code = stripslashes($verify_code);
-        $verify_code = mysql_real_escape_string($verify_code);
-        if (isset("summit")){
-            if($code == $verify_code){
-                header("reset.php");
-            }
-            else{
-                 echo "vertification code not match.";
-            }
-        }
 
     }
 }
-    function verify(){
+    function compare(){
     include(ConnectDatabase.php);
     // get the input
-    $email = $_POST["email"]; //input email
-    //prevent MySQL injection
-    $email = stripslashes($email);
-    $email = mysql_real_escape_string($email);
-    // generate code
-    //write query
-    $sql = "SELECT email_address FROM user
-    WHERE email_address = '$email' ";
-    //get result according to the query in database
-    $result = mysqli_query($connect,$sql);
-    //fetch the result from query into the asociative array format
-    $userdata = mysqli_fetch_all($result,MySQLI_ASSOC);
-    if ($result == NULL){
-        // red message of user not exist
-        echo "user not exist.";
-    }
-    else{
-        $code = substr(md5(uniqid(rand(), true)), 8, 8);
-        //email
-        $email_to = "$email";
-        $email_subject = "Please Verify Your AcadMap Account";
-        $header = "From: noreply@AcapMap.com";
-        $message = "Dear $user,/n
-            This mail is to verify your email address. Please enter the following verification code:
-                \n\n$code\n\nThank you.\n\nRegrads,\nThe AcadMap team";
-                
-        mail($email,$email_subject,$message,[$header]);
-        $verify_code = $_POST["code"]; //input = code
+    $verify_code = $_POST["code"]; //input = code
         $verify_code = stripslashes($verify_code);
         $verify_code = mysql_real_escape_string($verify_code);
         if (isset("summit")){
@@ -85,7 +54,6 @@
                  echo "vertification code not match.";
             }
         }
-
     }
  ?>
 
