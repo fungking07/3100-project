cda1c13aff2ae6a2dd959174a7e70183a94b6997
henryL0-0 <49423066+henryL0-0@users.noreply.github.com>
2020-04-19 22:08:37 +0800

    add

---
+++ verifcation.php
@@ -0,0 +1,177 @@
+<?php
+  //use helper function to connect to the database
+  function verify(){
+    include(ConnectDatabase.php);
+    // get the input
+    $email = $_POST["email"]; //input email
+    //prevent MySQL injection
+    $email = stripslashes($email);
+    $email = mysql_real_escape_string($email);
+    // generate code
+    //write query
+    $sql = "SELECT email_address FROM user
+    WHERE email_address = '$email' ";
+    //get result according to the query in database
+    $result = mysqli_query($connect,$sql);
+    //fetch the result from query into the asociative array format
+    $userdata = mysqli_fetch_all($result,MySQLI_ASSOC);
+    if ($result == NULL){
+        // red message of user not exist
+        echo "user not exist.";
+    }
+    else{
+        $code = substr(md5(uniqid(rand(), true)), 8, 8);
+        //email
+        $email_to = "$email";
+        $email_subject = "Please Verify Your AcadMap Account";
+        $header = "From: noreply@AcapMap.com";
+        $message = "Dear $user,/n
+            This mail is to verify your email address. Please enter the following verification code:
+                \n\n$code\n\nThank you.\n\nRegrads,\nThe AcadMap team";
+                
+        mail($email,$email_subject,$message,[$header]);
+        $verify_code = $_POST["code"]; //input = code
+        $verify_code = stripslashes($verify_code);
+        $verify_code = mysql_real_escape_string($verify_code);
+        if (isset("summit")){
+            if($code == $verify_code){
+                header("reset.php");
+            }
+            else{
+                 echo "vertification code not match.";
+            }
+        }
+
+    }
+}
+    function verify(){
+    include(ConnectDatabase.php);
+    // get the input
+    $email = $_POST["email"]; //input email
+    //prevent MySQL injection
+    $email = stripslashes($email);
+    $email = mysql_real_escape_string($email);
+    // generate code
+    //write query
+    $sql = "SELECT email_address FROM user
+    WHERE email_address = '$email' ";
+    //get result according to the query in database
+    $result = mysqli_query($connect,$sql);
+    //fetch the result from query into the asociative array format
+    $userdata = mysqli_fetch_all($result,MySQLI_ASSOC);
+    if ($result == NULL){
+        // red message of user not exist
+        echo "user not exist.";
+    }
+    else{
+        $code = substr(md5(uniqid(rand(), true)), 8, 8);
+        //email
+        $email_to = "$email";
+        $email_subject = "Please Verify Your AcadMap Account";
+        $header = "From: noreply@AcapMap.com";
+        $message = "Dear $user,/n
+            This mail is to verify your email address. Please enter the following verification code:
+                \n\n$code\n\nThank you.\n\nRegrads,\nThe AcadMap team";
+                
+        mail($email,$email_subject,$message,[$header]);
+        $verify_code = $_POST["code"]; //input = code
+        $verify_code = stripslashes($verify_code);
+        $verify_code = mysql_real_escape_string($verify_code);
+        if (isset("summit")){
+            if($code == $verify_code){
+                header("reset.php");
+            }
+            else{
+                 echo "vertification code not match.";
+            }
+        }
+
+    }
+ ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+		<style type="text/css">
+			*{
+				margin:0;
+				padding:0;
+			}
+			html{
+				background:url(img/reset.jpg) no-repeat center center fixed;
+				background-size: cover;
+			}
+			.Reset-Password{
+				margin-top:150px;
+				margin-left:37%;
+				font-size: 50px;
+				font-family: 'Cooper Black';
+				color:#8b7765;
+			}
+			#email{
+				margin-left:25%;
+				margin-top:80px;
+				width: 50%;
+				height:35px;
+				border-style: solid;
+				font-size: 20px;
+				border-radius: 20px;
+				border-color: gray;
+				padding-left: 1%;
+				outline: none;
+			}
+			#code{
+				display:inline-block;
+				margin-left:25%;
+				margin-top:50px;
+				width: 35%;
+				height:35px;
+				border-style: solid;
+				font-size: 20px;
+				border-radius: 20px;
+				border-color: gray;
+				padding-left: 1%;
+				outline: none;
+			}
+			#send{
+				display:inline-block;
+				margin-left:2%;
+				width: 13%;
+				height:42px;
+				border-radius: 20px;
+				padding-left: 1%;
+				border-style: solid;
+				font-size: 16px;
+				border-color: gray;
+				outline: none;
+				background-color: white;
+			}
+			.submit{
+				margin-left:47%;
+				margin-top:65px;
+				width: 10%;
+				height:35px;
+				border-radius: 20px;
+				text-align: center;
+				border-style: none;
+				background-color: #4876ff;
+				font-size: 16px;
+			}
+			
+		</style>
+	</head>
+	<body>
+		<div class="Reset-Password">
+			Forget Password?
+        </div>
+        <form action="verifcation.php">
+            <input type="text" name="email" id="email" placeholder="Email" />
+            <input type="submit" name="send" id="send" type="button" onclick="verify()">Email</button>
+            <input type="text" name="code" id="code" placeholder="Verification code" />
+            <input type="submit" name="send" id="send" type="button" onclick="verify()">Send</button>
+            <input class="submit" type="submit" value="submit" onclick="compare()"/>
+         </form>
+	</body>
+</html>
