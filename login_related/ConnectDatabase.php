f2e295ec43ff12c6d475311e295a7672113d46d4
angusking47 <56505603+angusking47@users.noreply.github.com>
2020-04-21 13:20:46 +0800

    login (finished) change Connect Data base to have session variable

--- login_related/ConnectDatabase.php
+++ login_related/ConnectDatabase.php
@@ -1,6 +1,6 @@
 <?php
   //connect to database as admin
- $connect = mysqli_connect("localhost","root","","csci3100");
+ $connect = mysqli_connect("localhost","root","","test");
  //check if connected
  if(!$connect){
    //output connection error if not connected.
--- login_related/login.php
+++ login_related/login.php
@@ -1,15 +1,23 @@
 <?php
   //use helper function to connect to the database
   include("ConnectDatabase.php");
+  $errors = array();
   function login_check(){
     global $connect;
+    global $errors;
     // get the input
     $username = $_POST["user"]; //input id = user
     $password = $_POST["pw"];
-    if($username == Null && $password == Null){
-      echo "Error!Login Failed!!!";
-      exit(1);
+
+    //if username and password is null
+    if (empty($username)) {
+    	array_push($errors, "Username is required");
     }
+    if (empty($password)) {
+    	array_push($errors, "Password is required");
+    }
+
+
     //prevent MySQL injection
     $username = stripslashes($username);
     $username = mysqli_real_escape_string($connect,$username);
@@ -17,34 +25,34 @@
     $password = mysqli_real_escape_string($connect,$password);
 
     //write query
-    $sql = "SELECT username,password, user_id FROM user
-    WHERE username = $username and password = $password";
+    $sql = "SELECT username,password,user_id FROM user
+    WHERE username = '$username' AND password = '$password'";
 
     //get result according to the query in database
     $result = mysqli_query($connect,$sql);
 
     //fetch the result from query into the asociative array format
-    $userdata = mysqli_fetch_all($result,MySQLI_ASSOC);
-    if ($userdata['username'] == $username && $userdata['password'] == $password){
-        $_SESSION["signed_in"] = true;
-        while($row = mysql_fetch_assoc($result)){
-          $_SESSION["username"] = $row["username"];
-          $_SESSION["user_id"] = $userdata["user_id"];
-        }
-        mysqli_free_result($result);
-        mysqli_close($connect);
-        header("forum.php");
+    $userdata = mysqli_fetch_assoc($result);
+
+    if($userdata != false){
+      if($userdata['username'] == $username && $userdata['password'] == $password){
+          $_SESSION["signed_in"] = true;
+          while($row = mysqli_fetch_assoc($result)){
+            $_SESSION["username"] = $row["username"];
+            $_SESSION["user_id"] = $row["user_id"];
+          }
+          mysqli_free_result($result);
+          mysqli_close($connect);
+          header("location:forum.php");
+    }
   }
   else{
     mysqli_free_result($result);
     mysqli_close($connect);
-    echo "Login fail. Please try it again.";
+    array_push($errors, "username/password is incorrect");
   }
 }
 
-  if(isset($_POST["forget"])){
-    header("verification.php");
-  }
   if(isset($_POST["register"])){
     header("register.php");
   }
@@ -68,11 +76,6 @@
 <link rel="stylesheet" href="../css/login.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="../js/bootstrap.min.js"></script>
-<script type="text/javascript">
-function redirect() {
-	window.location = "verification.php"
-}
-</script>
 </head>
 <nav class="navbar navbar-default navbar-fixed-top">
   <div class="container">
@@ -92,7 +95,7 @@ function redirect() {
 <div class="content">
   <div class="split left">
     <div class="centered">
-      <form  method="Post">
+      <form  method="Post" action = "login.php">
         <label for="fname">Username:</label>
         <input type="text" id="user" name="user"><br><br>
         <label for="lname">Password:</label>
@@ -100,7 +103,9 @@ function redirect() {
         <button type="submit" class="btn2" onclick="regPage()" name= "submit" value ="submit">CONFIRM</button>
         <a href="verification.php" class="btn2">Forget</a></button>
       </form>
+      <div class="red-text"><?php include("error.php")?></div>
     </div>
+
   </div>
 
   <div class="split right">
