f76e893a15649227bd3ade807e3abda53c4be286
KanaCS <49162615+KanaCS@users.noreply.github.com>
2020-04-22 23:54:56 +0800

    navbar xdddd

---
+++ navbar.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <a href="#" class="navbar-brand">AcadMap</a>
+      <div class="container-fluid">
+        <ul class="nav navbar-nav">
+          <li><a href="#">Forum</a></li>
+          <li><a href="#">Chat</a></li>
+          <li><a href="#">Consultation</a></li>
+          <!-- <input type="text" placeholder="Search.."> -->
+          <?php if($_SESSION["signed_in"] = true){
+              echo "<li><a href=‘#’> Profile</a></li>";
+              echo "<li><a href='#'>Welcome, ".$_SESSION['username']."!</a></li>";
+          }else{
+              echo "<li><a href='#'>Welcome, Visitor!</a></li>";
+          }
+          ?>
+        </ul>
+      </div>
+    </div>
+  </nav>
