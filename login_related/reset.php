7479205b1272d4858233d6d65e61bcdb2751af65
Hodge931 <59966421+Hodge931@users.noreply.github.com>
2020-04-22 20:19:23 +0800

    Update reset.php

--- login_related/reset.php
+++ login_related/reset.php
@@ -52,65 +52,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title></title>
-		<style type="text/css">
-			*{
-				margin:0;
-				padding:0;
-			}
-			html{
-				background:url(reset_bac.jpeg) no-repeat center center fixed;
-				background-size: cover;
-			}
-			.Reset-Password{
-				margin-top:150px;
-				margin-left:45%;
-				font-size: 50px;
-				font-family: 'Cooper Black';
-				color:#8b7765;
-			}
-			#new{
-				margin-left:25%;
-				margin-top:80px;
-				width: 50%;
-				height:35px;
-				border-style: solid;
-				font-size: 20px;
-				border-radius: 20px;
-				border-color: gray;
-				padding-left: 1%;
-				outline: none;
-			}
-			#confirm{
-				display:inline-block;
-				margin-left:25%;
-				margin-top:50px;
-				width: 50%;
-				height:35px;
-				border-style: solid;
-				font-size: 20px;
-				border-radius: 20px;
-				border-color: gray;
-				padding-left: 1%;
-				outline: none;
-			}
-      .red_text{
-        margin-left:44%;
-        margin-top:65px;
-        color: red;
-      }
-			.submit{
-				margin-left:44%;
-				margin-top:65px;
-				width: 10%;
-				height:35px;
-				border-radius: 20px;
-				text-align: center;
-				border-style: none;
-				background-color: #4876ff;
-				font-size: 16px;
-			}
-
-		</style>
+		<link rel="stylesheet" href="../css/reset.css">
 	</head>
 	<body>
 		<div class="Reset-Password">
