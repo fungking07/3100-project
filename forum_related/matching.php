9f662b8f4c3bed938c55bcdf55dff98d6670659a
henryL0-0 <49423066+henryL0-0@users.noreply.github.com>
2020-04-22 19:10:37 +0800

    Update matching.php

--- forum_related/matching.php
+++ forum_related/matching.php
@@ -5,5 +5,15 @@
         $user_result = mysql_query($connect,$usersql);
         $userinfo = mysql_fetch_array($find_result, MYSQLI_ASSOC);
         echo $userinfo["user_name"];
+        $allsql = 'SELECT user_name, eductional_level, major from user_profile';
+        $all_result = mysql_query($connect,$usersql);
+        $allinfo = mysql_fetch_array($find_result, MYSQLI_ASSOC);
+
+        $score = numarray
+        foreach($userinfo as $key => $oneuser){
+            if($oneuser["username"] != $user_result["username"]){
+                if()
+            }
+        }
     }
 ?>
