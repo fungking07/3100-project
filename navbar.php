288689ab146eff26f0ef775a75962c31d73c6575
KanaCS <49162615+KanaCS@users.noreply.github.com>
2020-04-23 00:05:29 +0800

    navbar debug

--- navbar.php
+++ navbar.php
@@ -7,9 +7,11 @@
           <li><a href="#">Chat</a></li>
           <li><a href="#">Consultation</a></li>
           <!-- <input type="text" placeholder="Search.."> -->
-          <?php if($_SESSION["signed_in"] = true){
-              echo "<li><a href=‘#’> Profile</a></li>";
+          <?php
+          if($_SESSION["signed_in"] == true){
+              
               echo "<li><a href='#'>Welcome, ".$_SESSION['username']."!</a></li>";
+              echo "<li><a href=‘#’> Logout</a></li>";
           }else{
               echo "<li><a href='#'>Welcome, Visitor!</a></li>";
           }
