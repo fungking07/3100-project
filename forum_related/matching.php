73af5e4805a2a7924cd83749e6378f14b4fe19a9
henryL0-0 <49423066+henryL0-0@users.noreply.github.com>
2020-04-23 00:50:19 +0800

    Update matching.php

--- forum_related/matching.php
+++ forum_related/matching.php
@@ -1,19 +1,34 @@
 <?php
     include("ConnectDatabase.php");
     function match(){
-        $usersql = 'SELECT user_name, eductional_level, major from user_profile WHERE user_name = '.$_SESSION["username"].'';
+        $usersql = 'SELECT user_name, eductional_level, faculty, institute from user_profile WHERE user_name = '.$_SESSION["username"].'';
         $user_result = mysql_query($connect,$usersql);
         $userinfo = mysql_fetch_array($find_result, MYSQLI_ASSOC);
         echo $userinfo["user_name"];
-        $allsql = 'SELECT user_name, eductional_level, major from user_profile';
+        $allsql = 'SELECT user_name, eductional_level, faculty, institute,consult_rating from user_profile';
         $all_result = mysql_query($connect,$usersql);
         $allinfo = mysql_fetch_array($find_result, MYSQLI_ASSOC);
 
-        $score = numarray
+        $score_arr = array();
+        $name_array = array();
         foreach($userinfo as $key => $oneuser){
-            if($oneuser["username"] != $user_result["username"]){
-                if()
+            $score = 0;
+            if($oneuser["username"] != $userinfo["username"]){
+                if($oneuser["eductional_level"] == $userinfo["educational_level"]){
+                    $score += 1;
+                }
+                if($oneuser["faculty"] == $userinfo["faculty"]){
+                    $score += 1;
+                }
+                if($oneuser["institute"] == $userinfo["institute"]){
+                    $score += 1;
+                }
+                $score += ($oneuser["consult_rating"] / 5);
             }
+            array_push($name_array, $oneuser["username"]);
+            array_push($score_arr, $score);
         }
+        $key = array_keys($score_arr, max($score_arr))[0];
+        echo $name_array[$key];
     }
 ?>
