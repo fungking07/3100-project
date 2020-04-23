6757ead4a151630bc831450458b3041050da8eac
Hodge931 <59966421+Hodge931@users.noreply.github.com>
2020-04-23 17:09:25 +0800

    introduction updated

--- login_related/introduction.php
+++ login_related/introduction.php
@@ -4,19 +4,42 @@
 		<meta charset="utf-8">
 		<title>Welcome to Acadmap</title>
 		<!--<link rel="stylesheet" href="../css/bootstrap.min.css">-->
-	  <link rel="stylesheet" href="../css/main.css">
-	   <link rel="stylesheet" href="../css/post.css">
+  <!--<link rel="stylesheet" href="../css/main.css">-->
+    <!--<link rel="stylesheet" href="../css/post.css">-->
 		<link rel="stylesheet" href="../css/introduction.css">
 	</head>
 	<body>
 		<?php include("../navbar.php"); ?>
-			<h1 class="intro_welcome">Welcome to Acadmap</h1>
-			<ul>
-				<li class="intro_content">Don’t know how to apply for famous university/institute?</li>
-				<li class="intro_content">Not clear about future study/internship?</li>
-				<li class="intro_content">Confused about what to prepare and what to focus on?</li>
-			</ul>
-			<h2 class="worry">NO WORRY!</h2>
-			<h3 class="professional">Experienced Professionals are here to help you!</h3>
+		<div class="intro_container">
+				<h1 class="intro_welcome">Welcome to Acadmap</h1>
+				<ul>
+
+					<li class="intro_content">If this is your first time to Acadmap,
+						you can browse the posts in the forum;
+						or you can register by clicking the "login"
+						button in the navagate bar.</li>
+						<li class="intro_content">The highlight of Acadmap is one-to-one tailored
+						professional consul-tation. To start a consultation,
+						you can click "match" button inside your profile.
+						We will find a suitable consultor to help you. Or,
+						you can also go to others' profile and consult him/her
+						directly.</li>
+						<li class="intro_content">Before consultation, you
+						may have a chat with the consultor to see if he
+						/she is suitable. If your agree to consult him/her, you
+					  need to pay some money. After your successful payment,
+						you can start a formal consult. In a formal consultion,
+						you can request more detailed and specific infor-mation
+						. After the consultation ends, your payment will
+						be transferred to the consultor's account. However, if you are
+					  unsatisfied with the consul-tation, you can contact the
+						admin and request the refund.</li>
+						<li class="intro_content">As a consultor, if you find the
+						the consultee does not end the meeting after your consultation
+						ends, we will try to connect the consultee and fix the problem.
+						However, the decision made by administor may not always suit your
+						test.</li>
+				</ul>
+		</div>
 	</body>
 </html>
