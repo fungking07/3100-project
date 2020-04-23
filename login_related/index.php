8fc774a39bd526242e23b4ace105aff71ce27ae9
henryL0-0 <49423066+henryL0-0@users.noreply.github.com>
2020-04-23 21:15:49 +0800

    Update index.php

--- login_related/index.php
+++ login_related/index.php
@@ -1,3 +1,7 @@
+<?php
+	session_start();
+	include("../navbar.php");
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -9,36 +13,17 @@
 		<link rel="stylesheet" href="../css/introduction.css">
 	</head>
 	<body>
-		<?php include("../navbar.php"); ?>
 		<div class="intro_container">
 				<h1 class="intro_welcome">Welcome to Acadmap</h1>
 				<ul>
 
-					<li class="intro_content">If this is your first time to Acadmap,
-						you can browse the posts in the forum;
-						or you can register by clicking the "login"
-						button in the navigate bar.</li>
-						<li class="intro_content">After you have registered, you can
-						log in to your account. But if you forget your password, you can reset
-					 	the password through email verifi-cation. After logging in, you may browse
-						the forum posts by categories, leave comments to posts, and even can chat with
-						the person who leaves the post.</li>
-						<li class="intro_content">The highlight of Acadmap is one-to-one tailored
-						professional consul-tation. To start a consultation,
-						you can click "match" button inside your profile.
-						We will find a suitable consultor to help you. Or,
-						you can also go to others' profile and consult him/her
-						directly.</li>
-						<li class="intro_content">Before consultation, you
-						may have a chat with the consultor to see if he
-						/she is suitable. If you agree to consult him/her, you
-					  need to pay some money. After your successful payment,
-						you can start a formal consult. In a formal consultion,
-						you can request more detailed and specific infor-mation.</li>
-						<li class="intro_content">After the consultation ends,
-						if the administor thinks the consultation is complete, your payment
-						will be transferred to the consultor's account. Otherwise the
-						payment will be given back to the your account.</li>
+					<li class="intro_content">If this is your first time to Acadmap, you can browse the posts in the forum; or you can register by clicking the "login" button in the navigate bar.</li>
+					<li class="intro_content">After you have registered, you can log in to your account. But if you forget your password, you can reset the password through email verification. After logging in, you may browse
+					the forum posts by categories, leave comments to posts, and even can chat with
+					the person who leaves the post.</li>
+					<li class="intro_content">The highlight of Acadmap is one-to-one tailored professional consultation. To start a consultation, you can click the "Match A Consult" button inside your profile. We will match a suitable consultor to help you. Also, you can go to others' profile and consult him/her directly.</li>
+					<li class="intro_content">Before the consultation begins, you can take a glance at the consultor's profile to gain some background information of the consultor. Then, you can chat with him/her to know more about him/her. These can be a reference for you to decide whether he/she is a suitable consultor for you. If you choose to consult him/her for a deeper, private and formal consultation, you can send a request to him/her and decide the consultation fee. If the consulter approve your request and your proposed fee, you can start the consultation with him/her in a consultation chatroom and have a formal consult. After the consultation, you can end the consultation chatroom and the chat record will be discarded automatically! The consultation fee will also be given to the consultor.</li>
+					<li class="intro_content">If you want to refund, or the consultee does not want to end the consult meeting, you can contact admin to hold your request. A request can be refunded from consultor, or get payment from consultee that is not willing to end the meeting. We, the AcadMap Team, will try our best to give you a reasonable result, but you have to take the risk that our decision may not be always what you want.</li>
 				</ul>
 		</div>
 	</body>
