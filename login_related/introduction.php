687039b00b394c65fc4828fdf34827f6a4e0e056
Hodge931 <59966421+Hodge931@users.noreply.github.com>
2020-04-23 18:09:02 +0800

    Update introduction.php

--- login_related/introduction.php
+++ login_related/introduction.php
@@ -17,7 +17,12 @@
 					<li class="intro_content">If this is your first time to Acadmap,
 						you can browse the posts in the forum;
 						or you can register by clicking the "login"
-						button in the navagate bar.</li>
+						button in the navigate bar.</li>
+						<li class="intro_content">After you have registered, you can
+						log in to your account. But if you forget your password, you can reset
+					 	the password through email verifi-cation. After logging in, you may browse
+						the forum posts by categories, leave comments to posts, and even can chat with
+						the person who leaves the post.</li>
 						<li class="intro_content">The highlight of Acadmap is one-to-one tailored
 						professional consul-tation. To start a consultation,
 						you can click "match" button inside your profile.
@@ -26,19 +31,14 @@
 						directly.</li>
 						<li class="intro_content">Before consultation, you
 						may have a chat with the consultor to see if he
-						/she is suitable. If your agree to consult him/her, you
+						/she is suitable. If you agree to consult him/her, you
 					  need to pay some money. After your successful payment,
 						you can start a formal consult. In a formal consultion,
-						you can request more detailed and specific infor-mation
-						. After the consultation ends, your payment will
-						be transferred to the consultor's account. However, if you are
-					  unsatisfied with the consul-tation, you can contact the
-						admin and request the refund.</li>
-						<li class="intro_content">As a consultor, if you find the
-						the consultee does not end the meeting after your consultation
-						ends, we will try to connect the consultee and fix the problem.
-						However, the decision made by administor may not always suit your
-						test.</li>
+						you can request more detailed and specific infor-mation.</li>
+						<li class="intro_content">After the consultation ends,
+						if the administor thinks the consultation is complete, your payment
+						will be transferred to the consultor's account. Otherwise the
+						payment will be given back to the your account.</li>
 				</ul>
 		</div>
 	</body>
