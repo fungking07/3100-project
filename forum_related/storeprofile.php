bd5e56d890c326198b97a07da39617989276cae0
angusking47 <56505603+angusking47@users.noreply.github.com>
2020-04-23 17:06:05 +0800

    Update storeprofile.php

--- forum_related/storeprofile.php
+++ forum_related/storeprofile.php
@@ -14,7 +14,19 @@
 	}
 
 	$edu_lv = $_GET['edu_lv'];
-	$name = $_GET['uname'];
+	if($name == $_GET['uname'];){
+		$flag = 0;
+	}
+	else{
+		$flag = 1;
+	}
+	if($email == $_GET['email'];){
+		$flag = 0;
+	}
+	else{
+		$flag = 2;
+	}
+
 	$prof = $_GET['prof'];
 	$inst = $_GET['inst'];
 	$email = $_GET['email'];
@@ -31,19 +43,36 @@
 	// echo 	$inst.'<br>';
 	// echo 	$email.'<br>';
 	// echo 	$major.'<br>';
-	$user_check_query = "SELECT * FROM user WHERE username = '$name' OR email_address = '$email'";
-	$result = mysqli_query($connect, $user_check_query);
-	if($result != False){
-			$user = mysqli_fetch_assoc($result);
-			if ($user){ // if user exists
-			if ($user['username'] == $name){
-				echo "Username already exists";
-			}
-			if ($user['email_address'] == $email){
-				echo "email already exists";
-			}
-		}
+	if(empty($_GET['uname'])){
+
 	}
+	else{
+		$name = $_GET['uname'];
+	}
+	if($flag == 1){
+		$username_check_query = "SELECT * FROM user WHERE username = '$name'";
+		$result = mysqli_query($connect, $username_check_query);
+		if($result != False){
+				$user = mysqli_fetch_assoc($result);
+				if ($user){ // if user exists
+				if ($user['username'] == $name){
+					echo "Username already exists";
+				}
+				}
+			}
+  	}
+		elseif ($flag == 2){
+			$useremail_check_query = "SELECT * FROM user WHERE email_address = '$email'";
+			$result = mysqli_query($connect, $useremail_check_query);
+			if($result != False){
+					$user = mysqli_fetch_assoc($result);
+					if ($user){ // if user exists
+					if ($user['email_address'] == $email){
+						echo "email already exists";
+					}
+					}
+				}
+		}
 
 
 	$commentsql = "UPDATE user_profile SET education_level='$edu_lv', username='$name', major='$major', personal_description='$prof', institute='$inst',cvv='$cvv', expire_mth='$exmth', expire_yr='$exyr', cardname='$cardname', cardnumber='$cardnumber'  WHERE user_id=$uid";
@@ -61,6 +90,7 @@
 	} else {
 	echo "Error: " . $commentsql . "<br>" . mysqli_error($conn)."<br>";
 	}
+	$_SESSION["username"] = $name;
 	echo "<script>history.go(-2);</script>";
 
 ?>
