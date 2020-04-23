b72da5215b165cbd4126cf41e74f9ab9658ef0c7
Hodge931 <59966421+Hodge931@users.noreply.github.com>
2020-04-23 12:18:36 +0800

    Introduction

---
+++ login_related/introduction.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Welcome to Acadmap</title>
+		<!--<link rel="stylesheet" href="../css/bootstrap.min.css">-->
+	  <link rel="stylesheet" href="../css/main.css">
+	   <link rel="stylesheet" href="../css/post.css">
+		<link rel="stylesheet" href="../css/introduction.css">
+	</head>
+	<body>
+		<?php include("../navbar.php"); ?>
+			<h1 class="intro_welcome">Welcome to Acadmap</h1>
+			<ul>
+				<li class="intro_content">Don’t know how to apply for famous university/institute?</li>
+				<li class="intro_content">Not clear about future study/internship?</li>
+				<li class="intro_content">Confused about what to prepare and what to focus on?</li>
+			</ul>
+			<h2 class="worry">NO WORRY!</h2>
+			<h3 class="professional">Experienced Professionals are here to help you!</h3>
+	</body>
+</html>
