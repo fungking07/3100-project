888c8ea180f7ccefb1b8ad19d1225f59c6933b97
angusking47 <56505603+angusking47@users.noreply.github.com>
2020-04-23 16:39:34 +0800

    update store

--- forum_related/storeprofile.php
+++ forum_related/storeprofile.php
@@ -31,6 +31,20 @@
 	// echo 	$inst.'<br>';
 	// echo 	$email.'<br>';
 	// echo 	$major.'<br>';
+	$user_check_query = "SELECT * FROM user WHERE username = '$name' OR email_address = '$email'";
+	$result = mysqli_query($connect, $user_check_query);
+	if($result != False){
+			$user = mysqli_fetch_assoc($result);
+			if ($user){ // if user exists
+			if ($user['username'] == $name){
+				array_push($errors, "Username already exists");
+			}
+			if ($user['email_address'] == $email){
+				array_push($errors, "email already exists");
+			}
+		}
+	}
+
 
 	$commentsql = "UPDATE user_profile SET education_level='$edu_lv', username='$name', major='$major', personal_description='$prof', institute='$inst',cvv='$cvv', expire_mth='$exmth', expire_yr='$exyr', cardname='$cardname', cardnumber='$cardnumber'  WHERE user_id=$uid";
 
