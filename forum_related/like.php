cd8e7db65c1062adf308ad7d30cd5fcfc3295d31
henry <49423066+henryL0-0@users.noreply.github.com>
2020-04-25 01:19:55 +0800

    addddddddddxd

--- forum_related/like.php
+++ forum_related/like.php
@@ -1,5 +1,10 @@
 <?php
   session_start();
+  if(isset($_SESSION['signed_in']) == false){
+    echo "<script> 
+    window.location.href='/403.php';
+    </script>";
+  }
   $servername = "localhost";
   $username = "root";
   $password = "";
@@ -13,6 +18,17 @@
   }
   $uid = $_SESSION['user_id'];
   $pid = $_SESSION['postid'];
+  $status = 0;
+  $sql = "SELECT liked FROM post_like WHERE user_id = $uid";
+  $result = mysqli_query($conn, $sql);
+  $info = mysqli_fetch_array($result);
+  if($info["liked"] == 1){
+    echo "<script> 
+    alert("You have liked this post. Thank you!");
+    </script>";
+    header("Location: {$_SERVER["HTTP_REFERER"]}");
+  }
+
   $likesql = "INSERT INTO post_like(user_id, liked, post_id) VALUES ($uid, 1, $pid)";
   $addsql1 = "UPDATE post_content set like_number = like_number + 1 where post_id = $pid";
   $addsql2 = "UPDATE forum set like_number = like_number + 1 where post_id = $pid";
@@ -20,7 +36,7 @@
   if (mysqli_multi_query($conn, $likesql) and mysqli_multi_query($conn, $addsql1) and mysqli_multi_query($conn, $addsql2)) {
     echo "New records created successfully";
   } else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+      echo "Error: sql error <br>" . mysqli_error($conn);
   }
    
   header("Location: {$_SERVER["HTTP_REFERER"]}");
